3959c5f73922813f8c88807cdb7ba5ae1c36f33c
QuantumCoderX <112435033+EzRa021@users.noreply.github.com>
2024-08-15 11:53:38 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,14 +5,45 @@ const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
+// Registration
+exports.register = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const user = await User.create({
+            fullName,
+            email,
+            password
+        });
+
+        res.cookie('token', generateToken(user._id), { httpOnly: true });
+        res.status(201).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Login
 exports.login = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.findOne({ email });
+
         if (user && (await user.matchPassword(password))) {
             res.cookie('token', generateToken(user._id), { httpOnly: true });
             res.json({
                 _id: user._id,
+                fullName: user.fullName,
                 email: user.email,
                 token: generateToken(user._id)
             });
